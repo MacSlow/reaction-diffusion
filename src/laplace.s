e990022f7fc226c890ebafc7b3c29da3652f843a
Mirco Müller <macslow@gmail.com>
2016-06-22 12:24:41 +0200

    Added comment header

--- src/laplace.s
+++ src/laplace.s
@@ -1,3 +1,27 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;3456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789
+;;
+;; reaction-diffusion simulation in 2D
+;;
+;; Copyright 2016 Mirco Müller
+;;
+;; Author(s):
+;;   Mirco "MacSlow" Müller <macslow@gmail.com>
+;;
+;; This program is free software: you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License version 3, as published
+;; by the Free Software Foundation.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranties of
+;; MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+;; PURPOSE.  See the GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License along
+;; with this program.  If not, see <http:;;www.gnu.org/licenses/>.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 global asmLaplaceSSE
 
 section .data
